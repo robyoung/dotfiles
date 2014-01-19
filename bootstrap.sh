1aac2823003b67335f26c0abc84365b68e27d48a
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-01-19 16:29:46 +0000

    Add bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,19 @@
 #
 # Run this script from this directory.
 
-DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Link config files into place
 for file in $(ls $DIR | grep -v 'bootstrap.sh'); do
   rm ~/.$file;
   ln -s $DIR/$file ~/.$file;
 done
+
+mkdir ~/.git_template
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+## Install NerdTREE
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
